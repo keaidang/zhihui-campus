6fd8a50dd48edb4fcaedb01d84108d01cb744851
keaidang <keaidang@gmail.com>
2026-09-20 08:50:03 +0800

    chore: 备案号更换为鲁ICP备2025186072号（首页/登录页/后台页脚三处）

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,7 +72,7 @@
 
     <footer class="site-footer">
       清北大学 · 智汇校园一站式服务平台 © 2026 ·
-      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">苏ICP备2026056678号</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">鲁ICP备2025186072号</a>
     </footer>
   </div>
 </template>
